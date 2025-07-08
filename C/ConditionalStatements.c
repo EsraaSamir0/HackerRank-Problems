7e7f8dc0bf2b6ad127d0ebc8c3bfed34c31f33a7
Esraa Samir <esraasamir609@gmail.com>
2025-07-08 23:52:18 +0300

    Passed All Test Cases

--- C/ConditionalStatements.c
+++ C/ConditionalStatements.c
@@ -20,17 +20,10 @@ int main()
 
     if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
 
+     char* num[10] = {"zero", "one", "two" ,"three", "four", "five", "six", "seven", "eight", "nine"};
+     
     if (n > 0 && n <= 9) {
-       switch (n) {
-       case 1 : printf("one"); break;
-       case 2 : printf("two"); break;
-       case 3 : printf("three"); break;
-       case 4 : printf("four"); break;
-       case 5 : printf("five"); break;
-       case 6 : printf("six"); break;
-       case 7 : printf("seven"); break;
-       case 8 : printf("eight"); break;
-       case 9 : printf("nine"); break;
+         printf("%s\n", num[n]);
        }
     }
     else {
