4a73f1b4a438d3b72b07808f906612ec133e52f1
Esraa Samir <esraasamir609@gmail.com>
2025-08-12 21:38:49 +0300

    Passed All Test Cases

---
+++ C++/Pointer.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void update(int *a,int *b) {
+    int diff;
+    diff = (*a > *b) ? (*a - *b) : (*b - *a);
+    *a = *a + *b;
+    *b = diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
